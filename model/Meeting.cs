ea45308c1156bf8c860ab4d8d8b051d549d2d930
eirikkar <eirikkar@proton.me>
2024-10-31 12:50:50 +0100

    added try catch to database functions

--- model/Meeting.cs
+++ model/Meeting.cs
@@ -26,113 +26,161 @@ class Meeting
 
     public SqliteConnection InitDatabase()
     {
-        var db = new SqliteConnection("Data Source=database/meeting.sqlite");
-        db.Open();
-        using (var cmd = db.CreateCommand())
+        try
         {
-            cmd.CommandText =
-                @"CREATE TABLE IF NOT EXISTS Meetings (
+            var db = new SqliteConnection("Data Source=database/meeting.sqlite");
+            db.Open();
+            using (var cmd = db.CreateCommand())
+            {
+                cmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS Meetings (
                                 id INTEGER PRIMARY KEY,
                                 meetingname TEXT NOT NULL,
                                 firstperson TEXT NOT NULL, 
                                 secondperson TEXT NOT NULL,
                                 date TEXT NOT NULL
                             );";
-            cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
+            }
+            return db;
         }
-        return db;
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int NewMeeting(Meeting meeting)
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var sq = db.CreateCommand();
-        sq.CommandText =
-            "INSERT INTO Meetings (MeetingName, FirstPerson, SecondPerson, Date) VALUES (@MeetingName, @FirstPerson, @SecondPerson, @Date)";
-        sq.Parameters.AddWithValue("@MeetingName", meeting.MeetingName);
-        sq.Parameters.AddWithValue("@FirstPerson", meeting.FirstPerson?.Name);
-        sq.Parameters.AddWithValue("@SecondPerson", meeting.SecondPerson?.Name);
-        sq.Parameters.AddWithValue("@Date", meeting.Date?.ToString(NorwegianCulture));
-        sq.ExecuteNonQuery();
-        sq.CommandText = "SELECT last_insert_rowid()";
-        int id = Convert.ToInt32(sq.ExecuteScalar());
-        return id;
+        try
+        {
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var sq = db.CreateCommand();
+            sq.CommandText =
+                "INSERT INTO Meetings (MeetingName, FirstPerson, SecondPerson, Date) VALUES (@MeetingName, @FirstPerson, @SecondPerson, @Date)";
+            sq.Parameters.AddWithValue("@MeetingName", meeting.MeetingName);
+            sq.Parameters.AddWithValue("@FirstPerson", meeting.FirstPerson?.Name);
+            sq.Parameters.AddWithValue("@SecondPerson", meeting.SecondPerson?.Name);
+            sq.Parameters.AddWithValue("@Date", meeting.Date?.ToString(NorwegianCulture));
+            sq.ExecuteNonQuery();
+            sq.CommandText = "SELECT last_insert_rowid()";
+            int id = Convert.ToInt32(sq.ExecuteScalar());
+            return id;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public List<Meeting> GetAllMeetings()
     {
-        var meetings = new List<Meeting>();
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var cmd = new SqliteCommand("SELECT * FROM Meetings", db);
-        using var reader = cmd.ExecuteReader();
-
-        while (reader.Read())
+        try
         {
-            var meeting = new Meeting(
-                reader.GetString(1),
-                new Person(reader.GetString(2)),
-                new Person(reader.GetString(3)),
-                DateTime.Parse(reader.GetString(4), NorwegianCulture)
-            )
-            {
-                Id = reader.GetInt32(0),
-            };
-            meetings.Add(meeting);
-        }
+            var meetings = new List<Meeting>();
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var cmd = new SqliteCommand("SELECT * FROM Meetings", db);
+            using var reader = cmd.ExecuteReader();
 
-        return meetings;
+            while (reader.Read())
+            {
+                var meeting = new Meeting(
+                    reader.GetString(1),
+                    new Person(reader.GetString(2)),
+                    new Person(reader.GetString(3)),
+                    DateTime.Parse(reader.GetString(4), NorwegianCulture)
+                )
+                {
+                    Id = reader.GetInt32(0),
+                };
+                meetings.Add(meeting);
+            }
+
+            return meetings;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int DeleteMeeting(int id)
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-
-        using (var sq = db.CreateCommand())
+        try
         {
-            sq.CommandText = "DELETE FROM Meetings WHERE Id = @Id";
-            sq.Parameters.AddWithValue("@Id", id);
-            sq.ExecuteNonQuery();
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+
+            using (var sq = db.CreateCommand())
+            {
+                sq.CommandText = "DELETE FROM Meetings WHERE Id = @Id";
+                sq.Parameters.AddWithValue("@Id", id);
+                sq.ExecuteNonQuery();
+            }
+            ReassignIds();
+            return id;
         }
-        ReassignIds();
-        return id;
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int GetMeetingCount()
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var sq = db.CreateCommand();
-        sq.CommandText = "SELECT COUNT(*) FROM Meetings";
-        int count = Convert.ToInt32(sq.ExecuteScalar());
-        return count;
+        try
+        {
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var sq = db.CreateCommand();
+            sq.CommandText = "SELECT COUNT(*) FROM Meetings";
+            int count = Convert.ToInt32(sq.ExecuteScalar());
+            return count;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public void ReassignIds()
     {
-        using var db = InitDatabase();
-        db.Open();
-        using var transaction = db.BeginTransaction();
-        var cmd = new SqliteCommand("SELECT id FROM Meetings ORDER BY id", db, transaction);
-        var reader = cmd.ExecuteReader();
-        int newId = 1;
-
-        while (reader.Read())
+        try
         {
-            int oldId = reader.GetInt32(0);
-            var updateCmd = new SqliteCommand(
-                "UPDATE Meetings SET id = @newId WHERE id = @oldId",
-                db,
-                transaction
-            );
-            updateCmd.Parameters.AddWithValue("@newId", newId);
-            updateCmd.Parameters.AddWithValue("@oldId", oldId);
-            updateCmd.ExecuteNonQuery();
-            newId++;
-        }
+            using var db = InitDatabase();
+            db.Open();
+            using var transaction = db.BeginTransaction();
+            var cmd = new SqliteCommand("SELECT id FROM Meetings ORDER BY id", db, transaction);
+            var reader = cmd.ExecuteReader();
+            int newId = 1;
 
-        transaction.Commit();
+            while (reader.Read())
+            {
+                int oldId = reader.GetInt32(0);
+                var updateCmd = new SqliteCommand(
+                    "UPDATE Meetings SET id = @newId WHERE id = @oldId",
+                    db,
+                    transaction
+                );
+                updateCmd.Parameters.AddWithValue("@newId", newId);
+                updateCmd.Parameters.AddWithValue("@oldId", oldId);
+                updateCmd.ExecuteNonQuery();
+                newId++;
+            }
+
+            transaction.Commit();
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 }
--- model/Person.cs
+++ model/Person.cs
@@ -14,116 +14,172 @@ class Person
 
     public SqliteConnection InitDatabase()
     {
-        var db = new SqliteConnection("Data Source=database/person.sqlite");
-        db.Open();
-        using (var cmd = db.CreateCommand())
+        try
         {
-            cmd.CommandText =
-                @"CREATE TABLE IF NOT EXISTS Persons (
+            var db = new SqliteConnection("Data Source=database/person.sqlite");
+            db.Open();
+            using (var cmd = db.CreateCommand())
+            {
+                cmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS Persons (
                                 id INTEGER PRIMARY KEY,
                                 name TEXT NOT NULL
                             );";
-            cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
+            }
+            return db;
         }
-        return db;
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int AddPerson(Person person)
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var sq = db.CreateCommand();
-        sq.CommandText = "INSERT INTO Persons (Name) VALUES (@Name)";
-        sq.Parameters.AddWithValue("@Name", person.Name);
-        sq.ExecuteNonQuery();
-        sq.CommandText = "SELECT last_insert_rowid()";
-        int id = Convert.ToInt32(sq.ExecuteScalar());
-        return id;
+        try
+        {
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var sq = db.CreateCommand();
+            sq.CommandText = "INSERT INTO Persons (Name) VALUES (@Name)";
+            sq.Parameters.AddWithValue("@Name", person.Name);
+            sq.ExecuteNonQuery();
+            sq.CommandText = "SELECT last_insert_rowid()";
+            int id = Convert.ToInt32(sq.ExecuteScalar());
+            return id;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public Person? GetPersonById(int id)
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var cmd = new SqliteCommand("SELECT * FROM Persons WHERE id = @Id", db);
-        cmd.Parameters.AddWithValue("@Id", id);
-        using var reader = cmd.ExecuteReader();
+        try
+        {
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var cmd = new SqliteCommand("SELECT * FROM Persons WHERE id = @Id", db);
+            cmd.Parameters.AddWithValue("@Id", id);
+            using var reader = cmd.ExecuteReader();
 
-        if (reader.Read())
-        {
-            return new Person(reader.GetString(1)) { Id = reader.GetInt32(0) };
+            if (reader.Read())
+            {
+                return new Person(reader.GetString(1)) { Id = reader.GetInt32(0) };
+            }
+            else
+            {
+                return null;
+            }
         }
-        else
+        catch (SqliteException e)
         {
-            return null;
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
         }
     }
 
     public List<Person> GetAllPersons()
     {
-        var persons = new List<Person>();
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var cmd = new SqliteCommand("SELECT * FROM Persons", db);
-        using var reader = cmd.ExecuteReader();
-
-        while (reader.Read())
+        try
         {
-            var person = new Person(reader.GetString(1)) { Id = reader.GetInt32(0) };
-            persons.Add(person);
-        }
+            var persons = new List<Person>();
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var cmd = new SqliteCommand("SELECT * FROM Persons", db);
+            using var reader = cmd.ExecuteReader();
 
-        return persons;
+            while (reader.Read())
+            {
+                var person = new Person(reader.GetString(1)) { Id = reader.GetInt32(0) };
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int DeletePerson(int id)
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-
-        using (var sq = db.CreateCommand())
+        try
         {
-            sq.CommandText = "DELETE FROM Persons WHERE Id = @Id";
-            sq.Parameters.AddWithValue("@Id", id);
-            sq.ExecuteNonQuery();
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+
+            using (var sq = db.CreateCommand())
+            {
+                sq.CommandText = "DELETE FROM Persons WHERE Id = @Id";
+                sq.Parameters.AddWithValue("@Id", id);
+                sq.ExecuteNonQuery();
+            }
+            ReassignIds();
+            return id;
         }
-        ReassignIds();
-        return id;
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public int GetPersonCount()
     {
-        using SqliteConnection db = InitDatabase();
-        db.Open();
-        using var sq = db.CreateCommand();
-        sq.CommandText = "SELECT COUNT(*) FROM Persons";
-        int count = Convert.ToInt32(sq.ExecuteScalar());
-        return count;
+        try
+        {
+            using SqliteConnection db = InitDatabase();
+            db.Open();
+            using var sq = db.CreateCommand();
+            sq.CommandText = "SELECT COUNT(*) FROM Persons";
+            int count = Convert.ToInt32(sq.ExecuteScalar());
+            return count;
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 
     public void ReassignIds()
     {
-        using var db = InitDatabase();
-        db.Open();
-        using var transaction = db.BeginTransaction();
-        var cmd = new SqliteCommand("SELECT id FROM Persons ORDER BY id", db, transaction);
-        var reader = cmd.ExecuteReader();
-        int newId = 1;
-
-        while (reader.Read())
+        try
         {
-            int oldId = reader.GetInt32(0);
-            var updateCmd = new SqliteCommand(
-                "UPDATE Persons SET id = @newId WHERE id = @oldId",
-                db,
-                transaction
-            );
-            updateCmd.Parameters.AddWithValue("@newId", newId);
-            updateCmd.Parameters.AddWithValue("@oldId", oldId);
-            updateCmd.ExecuteNonQuery();
-            newId++;
-        }
+            using var db = InitDatabase();
+            db.Open();
+            using var transaction = db.BeginTransaction();
+            var cmd = new SqliteCommand("SELECT id FROM Persons ORDER BY id", db, transaction);
+            var reader = cmd.ExecuteReader();
+            int newId = 1;
 
-        transaction.Commit();
+            while (reader.Read())
+            {
+                int oldId = reader.GetInt32(0);
+                var updateCmd = new SqliteCommand(
+                    "UPDATE Persons SET id = @newId WHERE id = @oldId",
+                    db,
+                    transaction
+                );
+                updateCmd.Parameters.AddWithValue("@newId", newId);
+                updateCmd.Parameters.AddWithValue("@oldId", oldId);
+                updateCmd.ExecuteNonQuery();
+                newId++;
+            }
+
+            transaction.Commit();
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine($"Database error: {e.Message}");
+            throw;
+        }
     }
 }
